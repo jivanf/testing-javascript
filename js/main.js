88559702e370cc5d5ec743dbf2da3ef7235ca3cf
Jivan <minecraftjivan@gmail.com>
2017-02-21 18:02:10 -0800

    using String methods like substr and indexOf

--- js/main.js
+++ js/main.js
@@ -1,56 +1,32 @@
-/* var timeTable = new Array();
- timeTable[0] = 12;
- timeTable[1] = 12 * 2;
- timeTable[2] = 12 * 3;
- timeTable[3] = 12 * 4;
- timeTable[4] = 12 * 5;
- timeTable[5] = 12 * 6;
- timeTable[6] = 12 * 7;
- timeTable[7] = 12 * 8;
- timeTable[8] = 12 * 9;
- timeTable[9] = 12 * 10;
- timeTable[10] = 12 * 11;
- timeTable[11] = 12 * 12;
+//USING SUBSTR METHOD
+var myString = "Hello world how are you?";
+var indexOfMyString = myString.indexOf("how");
+document.write(indexOfMyString + "<br>");
 
+indexOfMyString = myString.lastIndexOf("how");
+document.write(indexOfMyString + "<br>");
 
- var indexOfArray;
- */
+indexOfMyString = myString.substr(0, 5);
+document.write(indexOfMyString + "<br>");
 
-function timeTables() {
+//USING INDEXOF METHOD
+var longString = "JavaScript is cool. ";
+longString = longString + "I like JavaScript. ";
+longString = longString + "I always use JavaScript";
+var javaScriptCount = 0;
+var foundAtPosition = 0;
 
-    var timesTable = prompt("What time table do you want to see?");
-    var startValue = prompt("From what number should I start?");
-    var endValue = prompt("Until what number should I stop?");
+while(foundAtPosition != -1) {
+    foundAtPosition = longString.indexOf("JavaScript", foundAtPosition);
 
-    if(startValue > endValue) {
-        alert("The starting number has to be less than the ending number!");
+    if(foundAtPosition != -1) {
+        foundAtPosition++;
+        javaScriptCount++;
     }
-
-    for(; startValue <= endValue; startValue++) {
-
-        if(isNaN(timesTable)) {
-            alert("The time table needs to be a number...")
-            break;
-        }
-
-        if(isNaN(startValue)) {
-            alert("The starting value needs to be a number...")
-            break;
-        }
-
-        if(isNaN(endValue)) {
-            alert("The final value needs to be a number...")
-            break;
-        }
-
-        document.write(timesTable + " * " + startValue + " = " + startValue * timesTable + "<br>");
-    }
-
 }
 
-/* for(indexOfArray in timeTable) {
- document.write(timeTable[indexOfArray] + "\n");
- }
- */
 
-timeTables();
+document.write(longString + "<br>");
+document.write("<H3>Total occurrences of the word JavaScript: </H3>" + javaScriptCount + "<br>");
+
+
