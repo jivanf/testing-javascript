f7fbbd676043682e0529145a5df01ee259c77ba4
Jivan <minecraftjivan@gmail.com>
2017-02-27 12:03:59 -0800

    program that prints all the numbers from 1 to 100, except the ones that are divisible by 3.

--- js/main.js
+++ js/main.js
@@ -1,46 +1,8 @@
-
-//Checks what method was returned
-function methodChecking() {
-    //Returns a decimal number
-    var numberSelection  = prompt("Write a decimal number");
-
-    //Returns a method from the Math object
-    var methodSelection = prompt("Choose a method of the Math object");
-
-    switch (methodSelection) {
-
-        case "Math.round":
-            var roundNumber = Math.round(numberSelection);
-            document.write("The number " + numberSelection + " using the Math.round method returns: " +
-                roundNumber + "<br>");
-            break;
-
-        case "Math.ceil":
-            var ceilNumber = Math.ceil(numberSelection);
-            document.write("The number " + numberSelection + " using the Math.ceil method returns: " +
-                ceilNumber + "<br>");
-            break;
-
-        case "Math.floor":
-            var floorNumber = Math.ceil(numberSelection);
-            document.write("The number " + numberSelection + " using the Math.floor method returns: " +
-                floorNumber + "<br>");
-            break;
-
-        case "Math.sqrt":
-            var sqrtNumber = Math.sqrt(numberSelection);
-            document.write("The number " + numberSelection + " using the Math.sqrt method returns: " +
-                sqrtNumber + "<br>");
-            break;
-            return;
-
-
+var loopCounter;
+for(loopCounter = 0; loopCounter <= 100; loopCounter++) {
+    if(loopCounter % 3 == 0) {
+        continue;
     }
 
-
+    document.write(loopCounter + "<br>");
 }
-
-methodChecking();
-
-
-
