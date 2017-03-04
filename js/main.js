39cfe4a3895dcbb200ad516587499f3a03ca578a
Jivan <minecraftjivan@gmail.com>
2017-03-04 14:44:23 -0800

    program that prints some text forever

--- js/main.js
+++ js/main.js
@@ -1,50 +1,19 @@
-//Array of fruits
-var fruits = ["Banana", "Apple", "Mango", "Pear"];
-var joinFruits = fruits.join(", ");
 
-var moreFruits = ["Orange", "Watermelon", "Papaya", "Pineapple"];
-var joinMoreFruits = moreFruits.join(", ");
-
-var fruitsArray = document.getElementById("fruit").innerHTML = joinFruits;
-var moreFruitsArray = document.getElementById("moreFruits").innerHTML = joinMoreFruits;
-
-function sortingArrays() {
-
-    var fruits = ["Banana", "Apple", "Mango", "Pear"];
-
-    //Gets the ID of the <p> tag where the array will be displayed
-    var sort = document.getElementById("sort");
-
-    //Sorts the fruits array in alphabetical order
-    var sortFruits = fruits.sort();
-    sortFruits = fruits.join("<br>");
-
-    sort.innerHTML = sortFruits;
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var ms = today.getMilliseconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    ms = checkTime(ms)
+    document.write("Insert text here");
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
 }
 
-function reversingArrays() {
-    var fruits = ["Banana", "Apple", "Mango", "Pear"];
-    var reverse = document.getElementById("reverse");
-
-    //Reverses the order of the fruits array
-    var reverseFruits = fruits.reverse();
-    reverseFruits = fruits.join("<br>");
-
-    //Displays the array
-    reverse.innerHTML = reverseFruits;
-}
-
-function concatArrays() {
-
-    var fruits = ["Banana", "Apple", "Mango", "Pear"];
-    var moreFruits = ["Orange", "Watermelon", "Papaya", "Pineapple"];
-
-    var concat = document.getElementById("concat");
-
-    //Concatenates the fruits array with the moreFruits array
-    var concatFruits = fruits.concat(moreFruits);
-
-    concatFruits = concatFruits.join("<br>");
-
-    concat.innerHTML = concatFruits;
-}
+startTime();
