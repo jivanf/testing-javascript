c498537f53055b33c1fe6418502dea4889ad85f5
Jivan <minecraftjivan@gmail.com>
2017-03-04 10:54:24 -0800

    testing array methods like sort and reverse

--- js/main.js
+++ js/main.js
@@ -1,8 +1,50 @@
-var loopCounter;
-for(loopCounter = 0; loopCounter <= 100; loopCounter++) {
-    if(loopCounter % 3 == 0) {
-        continue;
-    }
+//Array of fruits
+var fruits = ["Banana", "Apple", "Mango", "Pear"];
+var joinFruits = fruits.join(", ");
 
-    document.write(loopCounter + "<br>");
+var moreFruits = ["Orange", "Watermelon", "Papaya", "Pineapple"];
+var joinMoreFruits = moreFruits.join(", ");
+
+var fruitsArray = document.getElementById("fruit").innerHTML = joinFruits;
+var moreFruitsArray = document.getElementById("moreFruits").innerHTML = joinMoreFruits;
+
+function sortingArrays() {
+
+    var fruits = ["Banana", "Apple", "Mango", "Pear"];
+
+    //Gets the ID of the <p> tag where the array will be displayed
+    var sort = document.getElementById("sort");
+
+    //Sorts the fruits array in alphabetical order
+    var sortFruits = fruits.sort();
+    sortFruits = fruits.join("<br>");
+
+    sort.innerHTML = sortFruits;
+}
+
+function reversingArrays() {
+    var fruits = ["Banana", "Apple", "Mango", "Pear"];
+    var reverse = document.getElementById("reverse");
+
+    //Reverses the order of the fruits array
+    var reverseFruits = fruits.reverse();
+    reverseFruits = fruits.join("<br>");
+
+    //Displays the array
+    reverse.innerHTML = reverseFruits;
+}
+
+function concatArrays() {
+
+    var fruits = ["Banana", "Apple", "Mango", "Pear"];
+    var moreFruits = ["Orange", "Watermelon", "Papaya", "Pineapple"];
+
+    var concat = document.getElementById("concat");
+
+    //Concatenates the fruits array with the moreFruits array
+    var concatFruits = fruits.concat(moreFruits);
+
+    concatFruits = concatFruits.join("<br>");
+
+    concat.innerHTML = concatFruits;
 }
