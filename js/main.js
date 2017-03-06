f4e43b3b39cda1ba348a1159fbd477e993a5abd6
Jivan <minecraftjivan@gmail.com>
2017-03-05 16:13:43 -0800

    answer to question 2 of chapter 4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-var nowDate = new Date();
-nowDate.setMonth(nowDate.getMonth() + 12);
-document.write(nowDate);
+var arr = [];
+for(var i = 0; i < 10; i++){
+
+    var result = prompt("Enter your name here");
+
+    if(!result){
+        alert("That's not valid");
+        break;
+    }
+
+    arr.push(result);
+    document.write(result + "<br>");
+}
