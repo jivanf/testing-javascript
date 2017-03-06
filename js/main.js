6943fcf5ab437084b780ebdbe180614ba0a6d4b1
Jivan <minecraftjivan@gmail.com>
2017-03-06 10:45:00 -0800

    answer to question 3 from chapter 4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-var arr = [];
-for(var i = 0; i < 10; i++){
-
-    var result = prompt("Enter your name here");
-
-    if(!result){
-        alert("That's not valid");
-        break;
-    }
-
-    arr.push(result);
-    document.write(result + "<br>");
+function fix(fixNumber, decimalPlaces) {
+    var div = Math.pow(10, decimalPlaces);
+    fixNumber = Math.round(fixNumber * div) / div;
+    return fixNumber.toFixed(3);
 }
+
+var number1 = prompt("Enter the number with decimal places you want to fix");
+var number2 = prompt("How many decimal places do you want?");
+
+document.write(number1 + " fixed to " + number2 + " decimal places is: ");
+document.write(fix(number1, number2));
