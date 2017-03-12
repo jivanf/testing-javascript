a5d4bf37eef27c91a539901f365982f3e544ce7b
Jivan <minecraftjivan@gmail.com>
2017-03-12 13:44:14 -0700

    using different elements in forms like text boxes and password boxes

--- js/main.js
+++
@@ -1,5 +0,0 @@
-function displayHrefs() {
-    var i;
-    for(i = 0; i <= document.links.length; i++) { alert("Link number " + parseInt(i + 1) + " of this website is: "
-    + document.links[i].href)}
-}
