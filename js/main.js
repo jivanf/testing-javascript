fc086acd2f7753468bdffd2b96d2a1566e150a9f
Jivan <minecraftjivan@gmail.com>
2017-03-11 18:50:05 -0800

    answer to question 3 of chapter 5

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
-function checkOS() {
-    var os = navigator.userAgent;
-    if(os.indexOf("Macintosh") != -1) {
-        os = "Macintosh"
-        location.href = "maconly.html"
+var myImages = ["img/usa.png", "img/mexico.png", "img/canada.png", "img/german.png"];
+var newImage = Math.round(Math.random() * 3);
 
+function onImage(imgNumber) {
+    var imgClicked = document.images[imgNumber];
+    while(imgClicked.src.indexOf(myImages[newImage]) != -1) {
+        newImage = Math.round(Math.random() * 3);
     }
 
-    else {
-        os = "Windows"
-        location.href = "windowsonly.html"
-    }
-
-    return os;
+    imgClicked.src = myImages[newImage]
 }
 
+function outImage(imgNumber) {
+    document.images[imgNumber].src = myImages[imgNumber];
+}
