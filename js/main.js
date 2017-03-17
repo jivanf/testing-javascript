4abe8857ed5a68ab58c27f8bd9c49dab877df6a2
Jivan <minecraftjivan@gmail.com>
2017-03-16 18:33:11 -0700

    create a form where user puts some information

--- js/main.js
+++ js/main.js
@@ -1,11 +1,47 @@
-var timesClicked = 0;
-function clickButtonAndDisplayNumber() {
-    timesClicked++;
+var form = document.form1;
+function nameValidation() {
+    if(form.name.value == "") {
+        form.name.style.backgroundColor = "#FF0000"
+    }
 
-    var number = document.getElementById("number").innerHTML = timesClicked;
+    else {
+        form.name.style.backgroundColor = "#008000"
+    }
+
 }
 
-function mouseUpOnButton() {
-     document.getElementById("onMouse").innerHTML = "You just clicked the 'click a button to make a text appear' button";
+function ageValidation() {
+    if(form.age.value == "") {
+        form.age.style.backgroundColor = "#FF0000"
+    }
+
+    else if(form.age.value > 100) {
+        form.age.style.backgroundColor = "#FF0000"
+    }
+
+    else {
+        form.age.style.backgroundColor = "#008000"
+    }
+
 }
 
+function buttonValidation() {
+    if (form.age.value > 100) {
+        alert("Please put your real age in the text box");
+    }
+
+    else if(form.age.value == "") {
+        alert("You need to put your age in the correct text box");
+    }
+
+    else if(form.name.value == "") {
+        alert("You need to put your name in the correct text box");
+    }
+
+    else {
+        alert("Thank you " + form.name.value + " for completing my form");
+    }
+
+
+
+}
