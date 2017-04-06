a24829b29da60a982eb54746d314757d1dca0412
Jivan <minecraftjivan@gmail.com>
2017-04-05 17:01:16 -0700

    answer to question 2 chapter 6 (user input)

--- js/main.js
+++ js/main.js
@@ -1,71 +1,15 @@
 
-var numberOfOptions = prompt("How many options do you want to create?");
-var onlyLetters = /^[a-zA-Z\-_ ’'‘]*$/.test(numberOfOptions);
-var list = document.getElementById("list");
+function fahrenheitToCentigrade() {
+var fahrenheit = document.getElementById("fahrenheit1").value;
+var centigrade;
 
-function createOptions() {
+centigrade = 5/9 * (fahrenheit - 32);
 
-    for (var i = 0; i <= numberOfOptions; i++) {
+var fixedCentigrade = centigrade.toFixed(2);
 
-        if (numberOfOptions > 10000) {
-
-            alert("Too many options!");
-            window.location.reload();
-            break;
-        }
-
-        if (numberOfOptions == 0) {
-            alert("You can't create zero options");
-            window.location.reload();
-            break;
-        }
-
-
-        if (i == 0) {
-            i = 1;
-        }
-
-        var option = document.createElement("option");
-        option.text = "Option #" + i;
-        list.add(option);
-
-    }
-
-    if (onlyLetters == true) {
-        alert("You can't put letters here");
-        window.location.reload();
-    }
+document.getElementById("centigrade1").value = fixedCentigrade;
 
 }
 
-function createRemoveSelectedButton() {
-    var div1 = document.getElementById("div1");
-    var button = '<button id="remove_selected_but" onclick="removeSelectedOption();">Remove selected options</button>'
-    div1.innerHTML = button;
-    
-}
-
-function removeSelectedOption() {
-    list.remove(list.selectedIndex);
-
-}
-
-function createRemoveButton() {
-
-    var div2 = document.getElementById("div2");
-    var button = '<button id="remove_but" onclick="removeOptions(list);">Remove options</button>'
-    div2.innerHTML = button;
-
-}
-
-function removeOptions(selectbox) {
-    var i;
-    for (i = selectbox.options.length - 1; i >= 0; i--) {
-        selectbox.remove(i);
-    }
-}
-
-createRemoveButton();
-createRemoveSelectedButton();
-createOptions();
+document.getElementById("fahrenheit_to_centigrade").addEventListener("click", fahrenheitToCentigrade);
 
