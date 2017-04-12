9a94c7391f4ec25d253a351717b641b330a646bd
Jivan <minecraftjivan@gmail.com>
2017-04-12 11:50:15 -0700

    using regexp with the split method

--- js/main.js
+++ js/main.js
@@ -1,30 +1,5 @@
-var txt = "A,B,C,D";
-var but = document.getElementById("but");
-var toggle = 0;
-var newTxt = txt.split(",");
-var p = document.getElementById("txt");
-
-function reverseText() {
-    newTxt.reverse();
-    toggle = 1;
-    p.innerHTML = newTxt;
-}
-
-if(toggle == 1) {
-
-    function originalText() {
-        newTxt.reverse();
-        p.innerHTML = newTxt
-    }
-}
-
-function callBothFunctions() {
-    reverseText();
-    originalText();
-}
-
-but.addEventListener("click", reverseText, originalText());
-
-
-
-
+var pricesAndFruits = "Apple, 0.99. Banana, 1. Pear, 0.50";
+var regExp = /[^a-z]+/i;
+var splitString = pricesAndFruits.split(regExp);
+document.write("The fruits are: <br>" + splitString.join("<br>"));
+alert(pricesAndFruits);
