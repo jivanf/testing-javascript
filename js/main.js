2cd44fb1d367a17a8d9915b29076cc6e9a031a15
Jivan <minecraftjivan@gmail.com>
2017-04-14 21:07:06 -0700

    answer to question 3 chapter 8

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var str = "a dog walked in off a street and ordered the finest beer";
-var regExp = /\ba\b/g;
-var res = str.replace(regExp, "the");
+var str = "Mmm, I love chocolate, I'm a chocolic. I love candy too, sweet, sugar candy.";
+var regExp = /candy|chocolic|chocolate|sugar candy/g;
+var res = str.replace(regExp, "salad");
 alert(res);
