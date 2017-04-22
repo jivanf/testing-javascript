68ff7a275e85f0fd81a056ab6042407d358b15f2
Jivan <minecraftjivan@gmail.com>
2017-04-22 12:54:04 -0700

    using the moment.js date library and intervals in a countdown

--- js/main.js
+++ js/main.js
@@ -1,108 +1,33 @@
-
-date = new Date();
-cities = null;
-minutesDiff = null;
-
+var counterButton = null;
+var counterHTML = null;
+var counterInt = 0;
+var counterID = null;
+var date = null;
 
 window.onload = function(){
-    date = new Date();
-    cities = document.getElementById("cities");
-
+    date = moment("2016-01-01T05:00:00-08:00");
+    counterButton = document.getElementById("stopCounter");
+    counterHTML = document.getElementById("counter");
+    counterButton.addEventListener("click", startCounter);
+    startCounter();
 }
 
+function startCounter() {
 
-function getSelectedValue(){
+    if(counterID){
+        clearInterval(counterID);
+        counterID = null;
+        counterButton.innerHTML = "Start Counter";
+        return true;
+    }
 
-    return cities.options[cities.selectedIndex].value;
+    counterID = setInterval(function(){
+        counterInt++;
+        date.subtract(1, 's');
+        counterHTML.innerHTML = date.format('hh:mm:ss');
+        console.log(date.toLocaleString());
+    }, 1000);
+
+    counterButton.innerHTML = "Stop Counter";
 
 }
-
-function checkSelection() {
-    if(cities.selectedIndex == -1) {
-        alert("Choose a city");
-        return false;
-    }
-
-    return true;
-}
-
-
-function printLocalTime() {
-
-    if (!checkSelection()) return false;
-
-    var localMinutes = date.getMinutes();
-    var localHours = date.getHours();
-    var localSeconds = date.getSeconds();
-
-    if(localMinutes < 10) {
-        localMinutes = "0" + localMinutes;
-    }
-
-    if(localHours < 10) {
-        localHours = "0" + localHours;
-    }
-
-    if(localSeconds < 10) {
-        localSeconds = "0" + localSeconds;
-    }
-
-    alert(localHours + ":" + localMinutes + ":" + localSeconds);
-
-}
-
-function printCityTime() {
-
-    if (!checkSelection()) return false;
-
-
-    var cityDate = new Date();
-
-    var minDiff = getSelectedValue();
-
-    var cityMinutes = date.getMinutes();
-    var cityHours = date.getHours();
-    var citySeconds = date.getSeconds();
-
-    if(cityMinutes < 10) {
-        cityMinutes = "0" + cityMinutes;
-    }
-
-    if(cityHours < 10) {
-        cityHours = "0" + cityHours;
-    }
-
-    if(citySeconds < 10) {
-        citySeconds = "0" + citySeconds;
-    }
-
-    cityDate.setMinutes(minDiff);
-
-    alert(cityDate.getHours() + ":" +cityDate.getMinutes() + ":" + cityDate.getSeconds());
-
-
-}
-
-
-function checkTime() {
-
-    if (!checkSelection()) return false;
-
-    var UTCMinutes = date.getMinutes();
-    var localHours = date.getHours();
-    var localSeconds = date.getSeconds();
-    var cityDate = new Date(date);
-
-    cityDate.setMinutes(date.getMinutes() + minutesDiff);
-
-    alert(new Date());
-    alert(cityDate);
-
-}
-
-document.getElementById("checkTime").addEventListener("click", checkTime);
-
-document.getElementById("printLocalTime").onclick = printLocalTime;
-
-document.getElementById("printCityTime").onclick = printCityTime;   
-
