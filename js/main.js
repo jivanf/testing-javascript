ce966b820a8fb65887a17deed75522730c80b585
Jivan <minecraftjivan@gmail.com>
2017-05-06 13:52:55 -0700

    using cookies to count how many times user visited website

--- js/main.js
+++ js/main.js
@@ -1,33 +1,73 @@
-var counterButton = null;
-var counterHTML = null;
-var counterInt = 0;
-var counterID = null;
-var date = null;
+var timesVisited = parseInt(getCookieValue("timesVisited"));
 
-window.onload = function(){
-    date = moment("2016-01-01T05:00:00-08:00");
-    counterButton = document.getElementById("stopCounter");
-    counterHTML = document.getElementById("counter");
-    counterButton.addEventListener("click", startCounter);
-    startCounter();
+
+function setCookie(cookieName, cookieValue) {
+    document.cookie = cookieName + "=" + cookieValue + "; expires=Thu, 18 Dec 2017 12:00:00 UTC";
 }
 
-function startCounter() {
-
-    if(counterID){
-        clearInterval(counterID);
-        counterID = null;
-        counterButton.innerHTML = "Start Counter";
-        return true;
+function getCookieValue(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
+    return "";
+}
 
-    counterID = setInterval(function(){
-        counterInt++;
-        date.subtract(1, 's');
-        counterHTML.innerHTML = date.format('hh:mm:ss');
-        console.log(date.toLocaleString());
-    }, 1000);
+function getCookieName(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) {
+            return null;
+        }
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+            end = dc.length;
+        }
+    }
+    return decodeURI(dc.substring(begin + prefix.length, end));
+}
 
-    counterButton.innerHTML = "Stop Counter";
+
+if(getCookieName("username") != null) {
+    document.getElementById("submit").remove();
+    document.getElementById("textbox").remove();
+}
+
+else {
+
+    document.getElementById("submit").addEventListener("click", function () {
+    setCookie("username", document.getElementById("textbox").value);
+    alert("Welcome " + "to my website" + ", " + getCookieValue("username"))
+    document.getElementById("submit").remove();
+    document.getElementById("textbox").remove();
+})
 
 }
+
+if (!timesVisited) {
+
+    timesVisited = 1;
+
+}
+else {
+
+    timesVisited = timesVisited += 1;
+    alert("Welcome back " + getCookieValue("username") + "!" + "  You have visited my website " +  timesVisited + " times");
+}
+
+setCookie("timesVisited", timesVisited);
