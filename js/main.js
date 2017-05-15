2617460b2afb97c1f4bfb1c1a54bd83a0fee524f
Jivan <minecraftjivan@gmail.com>
2017-05-15 11:09:40 -0700

    using methods from the node and document objects to navigate through the DOM

--- js/main.js
+++ js/main.js
@@ -1,51 +1,24 @@
+var html = document.documentElement;
+var head = html.firstElementChild;
+var body = head.nextElementSibling;
 
+var header = document.createElement("H1");
+var headerText = document.createTextNode("This is an H1");
+var paragraph = document.createElement("P");
+var paragraphText = document.createTextNode("This is a paragraph");
 
-window.onload = function () {
-    var imgUSA = document.getElementById("imgUSA");
-    var imgCanada = document.getElementById("imgCanada");
-    var imgGermany = document.getElementById("imgGermany");
+paragraph.appendChild(paragraphText);
+header.appendChild(headerText);
 
-    imgGermany.onmouseover = function() {showText(this.id)};
-    imgUSA.onmouseover = function() {showText(this.id)};
-    imgCanada.onmouseover = function() {showText(this.id)};
+body.appendChild(header);
+body.appendChild(paragraph);
 
-    imgGermany.onmouseout = function() {hideText(this.id)};
-    imgUSA.onmouseout = function() {hideText(this.id)};
-    imgCanada.onmouseout = function() {hideText(this.id)};
-
-
-}
-
-function showText(id) {
-    if (id == "imgGermany") {
-        document.getElementById("textGermany").innerText = "Learn more about Germany";
-    }
-
-    else if (id == "imgCanada" ) {
-        document.getElementById("textCanada").innerText = "Learn more about Canada";
-    }
-
-    else {
-        document.getElementById("textUSA").innerText = "Learn more about the USA";
-    }
-}
-
-function hideText(id) {
-    if (id == "imgGermany" ) {
-        document.getElementById("textGermany").innerText = "";
-    }
-
-    else if (id == "imgCanada") {
-        document.getElementById("textCanada").innerText = "";
-    }
-
-    else {
-        document.getElementById("textUSA").innerText = "";
-    }
-}
-
-
-
+alert("The DOM Structure is: ")
+alert(html.tagName);
+alert(head.tagName);
+alert(body.tagName);
+alert(header.tagName);
+alert(paragraph.tagName);
 
 
 
